0c82a638590f985b54a463675baa65bab39c45f7
sangar3 <55110746+sangar3@users.noreply.github.com>
2020-02-21 22:53:52 -0600

    done

--- RetroFPS/Assets/Scripts/AmmoPickup.cs
+++ RetroFPS/Assets/Scripts/AmmoPickup.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class AmmoPickup : MonoBehaviour
 {
     public int ammoAmount = 25;
-    
 
- 
-   
+    public AudioClip pickupfx;
 
-     void OnTriggerEnter2D(Collider2D other)
+
+
+    void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
             Debug.Log("trigger");
             PlayerController.instance.currentAmmo += ammoAmount;
-
+            PlayerController.instance.UpdateAmmoUI();
             Destroy(gameObject);
+            AudioManager.Instance.PlaySFX(pickupfx, 3.0f);
         }
         
     }
--- RetroFPS/Assets/Scripts/PlayerController.cs
+++ RetroFPS/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
@@ -14,62 +15,138 @@ public class PlayerController : MonoBehaviour
     public Camera viewcam;
 
 
+    public int currentHealth;
+    public int maxHealth =100;
+
+    
+    public bool HasDied;
+
     public GameObject bulletimpact;
     public int currentAmmo;
 
     public Animator gunAnim;
+    public Animator PlayerAnim;
+    public Text HealthText, ammoText;
 
+    public AudioClip shootfx;
+    public AudioClip takedamagefx;
 
-    private  void Awake()
+    private void Awake()
     {
         instance = this;
 
     }
 
+    void Start()
+    {
+        currentHealth = maxHealth;
+        HealthText.text = currentHealth.ToString() + "%";
+
+        ammoText.text = currentAmmo.ToString();
+    }
+
     void Update()
     {
-        //player movement 
-        moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-
-        Vector3 moveHorizontal = transform.up * -moveInput.x;
-
-        Vector3 moveVertical = transform.right * moveInput.y;
-
-
-        rb.velocity = (moveHorizontal + moveVertical) * movespeed;
-
-        // player  mouse view control 
-
-        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mousesensitivity;
-
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z - mouseInput.x); // X-axis 
-
-        viewcam.transform.localRotation = Quaternion.Euler(viewcam.transform.localRotation.eulerAngles + new Vector3(0f, mouseInput.y, 0f));
-
-
-        // player shooting using ray casting
-
-        if(Input.GetMouseButtonDown(0))
+        if(!HasDied)
         {
-            if(currentAmmo > 0)
-            {
-                Ray ray = viewcam.ViewportPointToRay(new Vector3(.5f, .5f, 0f));
-                RaycastHit hit;
-                if (Physics.Raycast(ray, out hit))//if player hits something
-                {
-                    //Debug.Log("looking at " + hit.transform.name);
-                    Instantiate(bulletimpact, hit.point, transform.rotation);
+            //player movement 
+            moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-                }
-                else
+            Vector3 moveHorizontal = transform.up * -moveInput.x;
+
+            Vector3 moveVertical = transform.right * moveInput.y;
+
+
+            rb.velocity = (moveHorizontal + moveVertical) * movespeed;
+
+            // player  mouse view control 
+
+            mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mousesensitivity;
+
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z - mouseInput.x); // X-axis 
+
+            viewcam.transform.localRotation = Quaternion.Euler(viewcam.transform.localRotation.eulerAngles + new Vector3(0f, mouseInput.y, 0f));
+
+
+            // player shooting using ray casting
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                
+                if (currentAmmo > 0)
                 {
-                    Debug.Log("looking at nothing");
+                    Ray ray = viewcam.ViewportPointToRay(new Vector3(.5f, .5f, 0f));
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit))//if player hits something
+                    {
+                        //Debug.Log("looking at " + hit.transform.name);
+                        Instantiate(bulletimpact, hit.point, transform.rotation);
+                        if (hit.transform.tag == "Enemy")
+                        {
+                            hit.transform.parent.GetComponent<EnemyController>().TakeDamage(); // calling the EC to take damage when raycast hits enemy
+                        }
+                    }
+                    else
+                    {
+                        Debug.Log("looking at nothing");
+                    }
+                    currentAmmo--;
+                    gunAnim.SetTrigger("Shoot");
+                    UpdateAmmoUI();
+                    AudioManager.Instance.PlaySFX(shootfx, 3.0f);
                 }
-                currentAmmo--;
-                gunAnim.SetTrigger("Shoot");
+
             }
-            
+
+            if(moveInput != Vector2.zero)
+            {
+                PlayerAnim.SetBool("isMoving", true);
+            }
+            else
+            {
+                PlayerAnim.SetBool("isMoving", false);
+            }
+        }
+        
+
+    }
+
+
+
+    public void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        AudioManager.Instance.PlaySFX(takedamagefx, 3.0f);
+        if (currentHealth <= 0)
+        {
+            SceneManager.LoadScene("GameOver");
+            LockCursor();
+            HasDied = true;
+            currentHealth = 0;
+        }
+        HealthText.text = currentHealth.ToString() + "%";
+    }
+
+    public void AddHealth(int healAmount)
+    {
+        currentHealth += healAmount;
+        if(currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
         }
 
+        HealthText.text = currentHealth.ToString() + "%";
+    }
+
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void UpdateAmmoUI()
+    {
+        ammoText.text = currentAmmo.ToString();
     }
 }
